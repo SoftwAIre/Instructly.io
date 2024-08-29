ee52a9ea30b09c742a6dd26156276bac70f60d7d
Christopher Manna <cmanna285@gmail.com>
2024-08-29 15:41:52 -0700

    debug is false for prod

--- project5/settings.py
+++ project5/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0dacmgrl700bvmep1h&#=@z=h*$wjuwo^#8injm%o*gd_-a8t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["3.128.160.87"]
 
